59f213eb5a7be662b9eff6a29dbe24b6b5d1289f
Aliaksandr <lipskialiakasandr@gmail.com>
2021-09-13 10:32:23 +0300

    ready 5 tasks

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+             <NavLink to='/pre-junior' activeClassName="active" className={s.link}>pre-junior</NavLink>
+            <NavLink to='/junior' activeClassName="active" className={s.link}>junior</NavLink>
+            <NavLink to='/junior-plus' activeClassName="active" className={s.link}>junior+</NavLink>
+            <div className={s.block}/>
         </div>
     )
 }

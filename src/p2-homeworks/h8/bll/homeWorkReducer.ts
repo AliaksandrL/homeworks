7299e56ab5ae0fcdbcc3f2a8e3a8ce450de8afd1
Aliaksandr <lipskialiakasandr@gmail.com>
2021-09-15 11:43:47 +0300

    ready 8 tasks, add style h2

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,19 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+type ActionType = { type: "sort", payload: 'up' | 'down' } | { type: "check", payload: number }
+
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
-            return state
+            const resultSort = [...state].sort((a, b) => a.name > b.name ? 1 : -1)
+            return action.payload === 'up' ? resultSort : resultSort.reverse()
         }
         case 'check': {
             // need to fix
-            return state
+            return state.filter(fl => fl.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
